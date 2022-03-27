dd56d72e24721b5c24f140e529ace376a46e8a31
thetime50 <1004763777@qq.com>
2022-03-28 01:47:44 +0800

    back

---
+++ crawler/README.md
@@ -0,0 +1,28 @@
+
+## 项目
+nvm node: 16.13.1
+
+调试
+
+编译
+
+运行
+
+ts-node 和babel-node 不建议在生产环境使用，因为页面是编译在内存中
+
+[让node支持es模块化(export、import)的方法](https://www.cnblogs.com/cag2050/p/7567248.html)  
+[babel 使用指南](https://www.babeljs.cn/docs/usage)  
+[babel-node](https://babeljs.io/docs/en/babel-node.html)  
+[You don’t need Babel with Node](https://blog.logrocket.com/you-dont-need-babel-with-node/)
+[node esm 模式](https://nodejs.org/api/esm.html#enabling) 在package.json中加入 type: 'module'
+
+
+es-lint
+
+## 蛋卷
+https://danjuanapp.com/  
+npm run dj
+
+## 天天
+npm run tt
+
---
+++ crawler/danjuan/index.js
@@ -0,0 +1,36 @@
+import {dataPath,userCookie} from './cfj.js';
+
+let obj = {a:1,b:2,c:{d:3,e:4}};
+
+obj.p1 = obj
+obj.p2 = obj.c
+obj.c.p3 = obj
+obj.c.p4 = obj.c
+
+console.dir(obj)
+
+// 登录测试
+
+import https from'https';
+
+// console.log('https', https)
+let url = "https://danjuanapp.com/djapi/fund/nav-growth/002213?day=360"
+// let url = "https://danjuanapp.com"
+
+async function main() {
+    let data = ''
+    let res = https.get(url,{headers:{
+        'Cookie': userCookie,
+        'Content-Type': 'application/json',
+    }}, (res) => {
+        console.log('res', res)
+        res.on('data', function(chunk){
+            console.log('- data', chunk.length)
+            data += chunk;
+        });
+        res.on('end', function(){
+            console.log(data);
+        });
+    })
+}
+main()
